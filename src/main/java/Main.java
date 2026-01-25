15db48bfe1382d55efa9ad8ad011e988dc006c2e
Mahmoud Gamal <mahmoudhaplas@gmail.com>
2026-01-25 11:50:01 +0200

    Add p2p handshake.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,23 +2,17 @@ import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
 
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLEncoder;
+import java.io.*;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.SecureRandom;
-import java.util.HexFormat;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 import com.dampcake.bencode.Bencode;
 import org.apache.commons.codec.Charsets;
@@ -70,7 +64,6 @@ public class Main {
 
             //  GET: /announce?peer_id=aaaaaaaaaaaaaaaaaaaa&info_hash=aaaaaaaaaaaaaaaaaaaa
             //  &port=6881&left=0&downloaded=100&uploaded=0&compact=1
-            StringBuilder getRequest = new StringBuilder();
 
             String announce = (String) meta.get("announce");
             String infoHashString = DigestUtils.sha1Hex(getInfoBytes(torrentBytes));
@@ -111,12 +104,54 @@ public class Main {
                         System.out.println();
                     }
                 }
-                else{
+                else {
                     System.err.println("Error in sending GET request");
                 }
 
             } catch (IOException e){
                 System.out.println(e.getMessage());
+            }
+
+        }
+        else if ("handshake".equals(command)){
+            byte[] torrentBytes = Files.readAllBytes(Path.of(args[1]));
+            String[] peer_info = args[2].split(":");
+
+            try{
+                Socket socket = new Socket(peer_info[0], Integer.parseInt(peer_info[1]));
+
+                // Building and sending Handshake
+                byte[] handshake = new byte[68];
+                handshake[0] = 19; // 1 Byte
+                System.arraycopy("BitTorrent protocol".getBytes(StandardCharsets.US_ASCII), 0,
+                                handshake, 1, 19); // 19 Byte
+                Arrays.fill(handshake, 20, 28, (byte) 0); // 8 Byte
+                byte[] infoHash = DigestUtils.sha1(getInfoBytes(torrentBytes));
+                System.arraycopy(infoHash, 0, handshake, 28, infoHash.length); // 20 Byte
+                System.arraycopy(peerId.getBytes(StandardCharsets.US_ASCII), 0, handshake, 48, 20);
+
+                OutputStream out = socket.getOutputStream();
+                InputStream in = socket.getInputStream();
+
+                out.write(handshake);
+                out.flush();
+
+                byte[] response = new byte[68];
+                int read = 0;
+                while(read < 68){
+                    // read() method doesn't guarantee full read,
+                    // so we perform while loop till we ensure reading 68 bytes
+                    int actualRead = in.read(response, read, 68 - read);
+                    if (actualRead == -1)
+                        throw new IOException("Peer closed connection during handshake");
+
+                    read += actualRead;
+                }
+
+                System.out.println("Peer ID: "+HexFormat.of().formatHex(Arrays.copyOfRange(response, 48, 68)));
+
+            } catch (Exception e){
+                System.err.println(e.getMessage());
             }
 
         }
